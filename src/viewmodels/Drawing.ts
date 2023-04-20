b9a18bdd0b56be068d3700fb915ff4e9c77b1f99
Jose Montana <ing.jmontana@gmail.com>
2023-04-20 17:17:45 -0500

    fix(selection tool): not unselecting previews shape

--- src/viewmodels/Drawing.ts
+++ src/viewmodels/Drawing.ts
@@ -46,6 +46,8 @@ export class DrawingViewModel {
   }
 
   setSelectedShape(shape: ShapeViewModel | null) {
+    this.selectedShape?.toggleSelected();
+
     this.selectedShape = shape;
     shape?.toggleSelected();
   }
